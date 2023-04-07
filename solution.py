c7043dccd8279b33eb56740e70d1437cf80ac37d
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-07 21:15:55 +0300

    qwerety

--- solution.py
+++ solution.py
@@ -6,14 +6,13 @@ from scipy.stats import norm
 
 chat_id = 287133833 # Ваш chat ID, не меняйте название переменной
 
+
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    # x =  2 * x / (5 * 5) 
     alpha = 1 - p
     loc = x.mean()
     scale = 1 / np.sqrt(len(x))
-    return 2/25*(loc + 1.5 - scale * norm.ppf(1 - alpha / 2)), \
-           2/25*(loc + 1.5 - scale * norm.ppf(alpha / 2))
-
+    return 2/25*(loc + 0.5 - scale * norm.ppf(1 - alpha / 2)), \
+           2/25*(loc + 0.5 - scale * norm.ppf(alpha / 2))
