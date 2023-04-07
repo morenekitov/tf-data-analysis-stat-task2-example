2d7d51318bba0a3286bcb7f813e26815f355f8b1
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-07 20:33:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,9 +10,10 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    x =  2 * x / (5 * 5) 
+    # x =  2 * x / (5 * 5) 
     alpha = 1 - p
-    loc = x.mean() 
+    loc = x.mean()
     scale = 1 / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    return 2/25*(loc + 1.5 - scale * norm.ppf(1 - alpha / 2)), \
+           2/25*(loc + 1.5 - scale * norm.ppf(alpha / 2))
+
